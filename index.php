a4bc3dcece0c052d9dde85c60b26dc783b506fa3
Steven Ruiz <ruizsteven22@gmail.com>
2026-02-09 19:46:01 -0300

    feat: Update theme metadata and redirect index.php
    Adds new tags and updates the version in style.css.
    Modifies index.php to include index.html, and updates templates/index.html to redirect to index.html, improving the initial entry point for the application.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Tierra Esperanza - Gestión de Comité
+ * Este archivo permite que la aplicación sea reconocida por servidores 
+ * que requieren un punto de entrada PHP.
+ */
+include_once('index.html');
+?>
